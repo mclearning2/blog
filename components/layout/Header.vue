f13d3ff30496cdd3fd5c954ac1d43288706702f5
mclearning2 <mclearning2@gmail.com>
2021-07-01 08:21:22 +0900

    site가 공사중임을 표시

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="site-header">
     <div class="site-header__cover">
-      <img src="~/assets/images/common/logo.png" alt="cover-image" />
+      <!--      <img src="~/assets/images/common/logo.png" alt="cover-image" />-->
     </div>
   </header>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,5 @@
 <template>
-  <div></div>
+  <div>
+    <h1>공사중 (~2021.07.04)</h1>
+  </div>
 </template>
