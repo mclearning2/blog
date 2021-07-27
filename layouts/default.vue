2a6027d579021f6a1d65494d238fe7c9dd8729d2
mclearning2 <mclearning2@gmail.com>
2021-07-28 01:03:34 +0900

    구글 애널리틱스 버그 수정

--- layouts/default.vue
+++ layouts/default.vue
@@ -28,7 +28,7 @@ export default {
     this.$store.commit('setRouteNames', this.$router.options.routes);
     this.$store.commit('setDockItems', this.$router.options.routes);
 
-    googleAnalytics();
+    this.googleAnalytics();
   },
 };
 </script>
